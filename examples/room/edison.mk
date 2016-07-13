56ad60bb837b809909bfd526a4a5acf8f98c4ddd
Gregg Reynolds <reynolds-gregg@norc.org>
2016-07-13 18:57:32 -0500

    makefile fixes
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- examples/room/edison.mk
+++ examples/room/edison.mk
@@ -10,7 +10,7 @@ INC=-I$(IOTIVITY_HOME)/resource/include \
 	-I$(IOTIVITY_HOME)/resource/csdk/logger/include \
 	-I$(IOTIVITY_HOME)/resource/oc_logger/include
 
-CXXFLAGS = -std=c++11 -DWITH_POSIX -D__linux__ -m32 -fPIC -Wall -Wextra -Wno-unused -Os -pthread $(INC)
+CXXFLAGS = -g -std=c++11 -DWITH_POSIX -D__linux__ -m32 -fPIC -Wall -Wextra -Wno-unused -Os -pthread $(INC)
 
 LDFLAGS=-L$(IOTIVITY_LIBPATH)
 
@@ -23,4 +23,4 @@ roomserver: roomserver.o
 roomrserver.o: roomrserver.cpp
 
 clean:
-	rm *.o *.dat roomserver roomclient
+	rm *.o roomserver roomclient
